d987021e17fa39084b9d6524692919dd9fc18ce8
dalvifaraz <dalvifaraz@gmail.com>
2025-07-12 00:27:07 +0530

    add util to generate 6 digit code for email verification model

--- src/common/utils/uuid.ts
+++ src/common/utils/uuid.ts
@@ -3,3 +3,7 @@ import { randomInt } from 'crypto';
 export function generateUniqueCode(): string {
   return randomInt(100000, 1000000).toString();
 }
+
+export function generateSixDigitCode(): string {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
